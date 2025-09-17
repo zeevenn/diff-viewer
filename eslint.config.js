6ccc3ea1822d7717038cab7ecd699127068a0ea2
Ziven <contact@ziven.me>
2025-09-17 17:08:56 +0800

    chore: use prettierrc in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { globalIgnores, defineConfig } from 'eslint/config';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import perfectionist from 'eslint-plugin-perfectionist';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores, defineConfig } from 'eslint/config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import perfectionist from 'eslint-plugin-perfectionist'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -30,7 +30,15 @@ export default defineConfig([
       'perfectionist/sort-exports': 'error',
       'perfectionist/sort-named-exports': 'error',
       'perfectionist/sort-named-imports': 'error',
+
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
     },
   },
   eslintPluginPrettierRecommended,
-]);
+])
