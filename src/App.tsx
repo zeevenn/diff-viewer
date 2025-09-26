2c63400e98685516192188cb74bb1ce3c42b0349
Ziven <contact@ziven.me>
2025-09-26 20:43:14 +0800

    feat: add toaster with sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 
+import { Toaster } from 'sonner'
 import { Layout } from './components/layout'
 import { ComingSoon, NotFound } from './components/ui'
 import { useDynamicFavicon } from './hooks/useDynamicFavicon'
+import { useTheme } from './hooks/useTheme'
 import { TextDiff } from './pages'
 
 function App() {
   useDynamicFavicon()
+  const { theme } = useTheme()
 
   return (
     <BrowserRouter>
       <Layout>
+        <Toaster position="top-center" richColors theme={theme} />
         <Routes>
           <Route path="/" element={<TextDiff />} />
           <Route path="/image" element={<ComingSoon />} />
