b6112c86ad9a9f78cf6ebbf6a66a9cfd5b267f97
ziven <contact@ziven.me>
2025-12-08 14:42:54 +0800

    chore: update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from '@zeevenn/eslint-config'
+import { antfu } from '@antfu/eslint-config'
 
-export default defineConfig({
+export default antfu({
   type: 'app',
   react: true,
   typescript: true,
+  rules: {
+    // Disable pnpm catalog validation rule
+    'pnpm/json-valid-catalog': 'off',
+  },
 })
